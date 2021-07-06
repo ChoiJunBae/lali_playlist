577f1168292f164ba7fd61266fdd43eca4fea717
ChoiJunBae <cjb9610@naver.com>
2021-07-06 18:13:15 +0900

    프로젝트 생성

---
+++ Lali-Play/src/main/java/com/example/Lali/Play/LaliPlayApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lali.Play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaliPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaliPlayApplication.class, args);
+	}
+
+}
